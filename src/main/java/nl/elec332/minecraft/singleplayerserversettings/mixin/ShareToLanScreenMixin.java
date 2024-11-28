c7243e3c051aa25eb72b2aa19e857cc3f9708765
Elec332 <6115944+Elec332@users.noreply.github.com>
2024-11-28 23:38:18 +0100

    1.20.x/1.21.x update

--- src/main/java/nl/elec332/minecraft/singleplayerserversettings/mixin/ShareToLanScreenMixin.java
+++ src/main/java/nl/elec332/minecraft/singleplayerserversettings/mixin/ShareToLanScreenMixin.java
@@ -7,10 +7,8 @@ import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.gui.screens.ShareToLanScreen;
 import net.minecraft.client.server.IntegratedServer;
 import net.minecraft.network.chat.Component;
-import org.spongepowered.asm.mixin.Final;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.*;
+import org.spongepowered.asm.mixin.gen.Accessor;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -28,10 +26,6 @@ public abstract class ShareToLanScreenMixin extends Screen {
         throw new RuntimeException();
     }
 
-    @Shadow
-    private int port;
-    @Shadow
-    private EditBox portEdit;
     @Unique
     private boolean onlineMode;
     @Unique
@@ -45,19 +39,25 @@ public abstract class ShareToLanScreenMixin extends Screen {
     private static final Component PVP_ALLOWED = Component.literal("PvP Allowed");
     @Unique
     private static final Component MOTD = Component.literal("MOTD");
-    @Final @Shadow //TODO: Find out why @Shadow doesn't work, but it's midnight and I can't be fucked atm...
-    private static final Component PORT_INFO_TEXT = Component.translatable("lanServer.port");
+//    @Final @Shadow //TODO: Find out why @Shadow doesn't work, but it's midnight and I can't be fucked atm...
+    @Unique
+    private static final Component PORT_INFO_TEXT_ = Component.translatable("lanServer.port");
 
+    @Accessor("portEdit")
+    protected abstract EditBox getBox();
+
+    @Accessor("port")
+    protected abstract void setPort(int i);
 
     @Inject(method = "init", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/components/EditBox;setResponder(Ljava/util/function/Consumer;)V", shift = At.Shift.AFTER))
     private void initHook(CallbackInfo ci) {
         IntegratedServer integratedServer = Objects.requireNonNull(Objects.requireNonNull(this.minecraft).getSingleplayerServer());
-        this.port = 25565;
+        this.setPort(25565);
         this.motd = integratedServer.getMotd();
         this.onlineMode = integratedServer.usesAuthentication();
         this.pvpAllowed = integratedServer.isPvpAllowed();
-        this.portEdit.setX(this.width / 2 + 5);
-        this.portEdit.setY(170);
+        this.getBox().setX(this.width / 2 + 5);
+        this.getBox().setY(170);
         this.addRenderableWidget(CycleButton.onOffBuilder(!this.onlineMode).create(this.width / 2 + 5, 130, 150, 20, OFFLINE_MODE, (cycleButton, b) -> this.onlineMode = !b));
         this.addRenderableWidget(CycleButton.onOffBuilder(this.pvpAllowed).create(this.width / 2 - 155, 130, 150, 20, PVP_ALLOWED, (cycleButton, b) -> this.pvpAllowed = b));
         EditBox motd = new EditBox(this.font, this.width / 2 - 155, 170, 150, 20, MOTD);
@@ -66,7 +66,7 @@ public abstract class ShareToLanScreenMixin extends Screen {
         this.addRenderableWidget(motd);
     }
 
-    @Inject(method = {"lambda$init$2", "method_19851"}, at = @At(value = "TAIL"), remap = false)
+    @Inject(method = {"lambda$init$2", "method_19851", "m_279789_"}, at = @At(value = "TAIL"), remap = false)
     private void onServerStart(CallbackInfo ci) {
         IntegratedServer integratedServer = Objects.requireNonNull(Objects.requireNonNull(this.minecraft).getSingleplayerServer());
         integratedServer.setUsesAuthentication(onlineMode);
@@ -78,7 +78,7 @@ public abstract class ShareToLanScreenMixin extends Screen {
 
     @Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphics;drawCenteredString(Lnet/minecraft/client/gui/Font;Lnet/minecraft/network/chat/Component;III)V", ordinal = 1, shift = At.Shift.AFTER), cancellable = true)
     private void render(GuiGraphics guiGraphics, int $$1, int $$2, float $$3, CallbackInfo ci) {
-        guiGraphics.drawCenteredString(this.font, PORT_INFO_TEXT, this.width / 2 + 80, 157, 16777215);
+        guiGraphics.drawCenteredString(this.font, PORT_INFO_TEXT_, this.width / 2 + 80, 157, 16777215);
         guiGraphics.drawCenteredString(this.font, MOTD, this.width / 2 - 80, 157, 16777215);
         ci.cancel();
     }
